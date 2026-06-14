dccd4ce4ca1e0caf56058c4351566d94ec6e7013
19961023 <107099833+19961023@users.noreply.github.com>
2026-06-14 18:32:21 +0800

    1

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-var CACHE_NAME = 'sewing-v1';
-var CACHE_URLS = ['index.html'];
+var CACHE_NAME = 'sewing-v2';
+var CACHE_URLS = [
+    'index.html',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
 
 self.addEventListener('install', function(e) {
     e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
         return cache.addAll(CACHE_URLS);
+    }));
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(caches.keys().then(function(keys) {
+        return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
     }));
 });
 
